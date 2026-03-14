60ee4a39330ec0170493a40f5c50afa73118eaa0
hoai90213-alt <hoai90213-alt@users.noreply.github.com>
2026-03-14 18:41:00 +0700

    Initial import and comprehensive rework fixes

---
+++ Natives/installer/modpack/ModrinthAPI.m
@@ -0,0 +1,169 @@
+#import "MinecraftResourceDownloadTask.h"
+#import "ModrinthAPI.h"
+#import "PLProfiles.h"
+
+@implementation ModrinthAPI
+
+- (instancetype)init {
+    return [super initWithURL:@"https://api.modrinth.com/v2"];
+}
+
+- (NSMutableArray *)searchModWithFilters:(NSDictionary<NSString *, NSString *> *)searchFilters previousPageResult:(NSMutableArray *)modrinthSearchResult {
+    int limit = 50;
+
+    NSMutableString *facetString = [NSMutableString new];
+    [facetString appendString:@"["];
+    [facetString appendFormat:@"[\"project_type:%@\"]", searchFilters[@"isModpack"].boolValue ? @"modpack" : @"mod"];
+    if (searchFilters[@"mcVersion"].length > 0) {
+        [facetString appendFormat:@",[\"versions:%@\"]", searchFilters[@"mcVersion"]];
+    }
+    [facetString appendString:@"]"];
+
+    NSDictionary *params = @{
+        @"facets": facetString,
+        @"query": [searchFilters[@"name"] stringByReplacingOccurrencesOfString:@" " withString:@"+"],
+        @"limit": @(limit),
+        @"index": @"relevance",
+        @"offset": @(modrinthSearchResult.count)
+    };
+    NSDictionary *response = [self getEndpoint:@"search" params:params];
+    if (!response) {
+        return nil;
+    }
+
+    NSMutableArray *result = modrinthSearchResult ?: [NSMutableArray new];
+    for (NSDictionary *hit in response[@"hits"]) {
+        BOOL isModpack = [hit[@"project_type"] isEqualToString:@"modpack"];
+        [result addObject:@{
+            @"apiSource": @(1), // Constant MODRINTH
+            @"isModpack": @(isModpack),
+            @"id": hit[@"project_id"],
+            @"title": hit[@"title"],
+            @"description": hit[@"description"],
+            @"imageUrl": hit[@"icon_url"]
+        }.mutableCopy];
+    }
+    self.reachedLastPage = result.count >= [response[@"total_hits"] unsignedLongValue];
+    return result;
+}
+
+- (void)loadDetailsOfMod:(NSMutableDictionary *)item {
+    NSArray *response = [self getEndpoint:[NSString stringWithFormat:@"project/%@/version", item[@"id"]] params:nil];
+    if (!response) {
+        return;
+    }
+    NSMutableArray<NSString *> *names = [NSMutableArray new];
+    NSMutableArray<NSString *> *mcNames = [NSMutableArray new];
+    NSMutableArray<NSString *> *urls = [NSMutableArray new];
+    NSMutableArray<NSString *> *hashes = [NSMutableArray new];
+    NSMutableArray<NSNumber *> *sizes = [NSMutableArray new];
+    [response enumerateObjectsUsingBlock:^(NSDictionary *version, __unused NSUInteger i, BOOL *stop) {
+        NSString *versionName = version[@"name"];
+        NSDictionary *file = [version[@"files"] firstObject];
+        NSString *url = file[@"url"];
+        if (versionName.length == 0 || url.length == 0) {
+            return;
+        }
+
+        [names addObject:versionName];
+        [mcNames addObject:[version[@"game_versions"] firstObject] ?: @""];
+        [sizes addObject:file[@"size"] ?: @0];
+        [urls addObject:url];
+        NSDictionary *hashesMap = file[@"hashes"];
+        [hashes addObject:hashesMap[@"sha1"] ?: @""];
+    }];
+    if (names.count == 0) {
+        self.lastError = [NSError errorWithDomain:@"ModrinthAPIErrorDomain"
+                                             code:1
+                                         userInfo:@{NSLocalizedDescriptionKey: @"No downloadable versions were found for this project."}];
+        return;
+    }
+
+    item[@"versionNames"] = names;
+    item[@"mcVersionNames"] = mcNames;
+    item[@"versionSizes"] = sizes;
+    item[@"versionUrls"] = urls;
+    item[@"versionHashes"] = hashes;
+    item[@"versionDetailsLoaded"] = @(YES);
+}
+
+- (void)downloader:(MinecraftResourceDownloadTask *)downloader submitDownloadTasksFromPackage:(NSString *)packagePath toPath:(NSString *)destPath {
+    NSError *error;
+    UZKArchive *archive = [[UZKArchive alloc] initWithPath:packagePath error:&error];
+    if (error) {
+        [downloader finishDownloadWithErrorString:[NSString stringWithFormat:@"Failed to open modpack package: %@", error.localizedDescription]];
+        return;
+    }
+
+    NSData *indexData = [archive extractDataFromFile:@"modrinth.index.json" error:&error];
+    NSDictionary* indexDict = [NSJSONSerialization JSONObjectWithData:indexData options:kNilOptions error:&error];
+    if (error) {
+        [downloader finishDownloadWithErrorString:[NSString stringWithFormat:@"Failed to parse modrinth.index.json: %@", error.localizedDescription]];
+        return;
+    }
+
+    downloader.progress.totalUnitCount = [indexDict[@"files"] count];
+    for (NSDictionary *indexFile in indexDict[@"files"]) {
+/*
+        if ([indexFile[@"downloads"] count] > 1) {
+            [downloader finishDownloadWithErrorString:[NSString stringWithFormat:@"Unhandled multiple files download %@", indexFile[@"downloads"]]];
+            return;
+        }
+*/
+        NSString *url = [indexFile[@"downloads"] firstObject];
+        NSString *relativePath = indexFile[@"path"];
+        if (url.length == 0 || relativePath.length == 0) {
+            downloader.progress.completedUnitCount++;
+            continue;
+        }
+        NSString *sha = indexFile[@"hashes"][@"sha1"];
+        NSString *path = [destPath stringByAppendingPathComponent:relativePath];
+        NSUInteger size = [indexFile[@"fileSize"] unsignedLongLongValue];
+        NSURLSessionDownloadTask *task = [downloader createDownloadTask:url size:size sha:sha altName:nil toPath:path];
+        if (task) {
+            [task resume];
+        } else if (!downloader.progress.cancelled) {
+            downloader.progress.completedUnitCount++;
+        } else {
+            return; // cancelled
+        }
+    }
+
+    [ModpackUtils archive:archive extractDirectory:@"overrides" toPath:destPath error:&error];
+    if (error) {
+        [downloader finishDownloadWithErrorString:[NSString stringWithFormat:@"Failed to extract overrides from modpack package: %@", error.localizedDescription]];
+        return;
+    }
+
+    [ModpackUtils archive:archive extractDirectory:@"client-overrides" toPath:destPath error:&error];
+    if (error) {
+        [downloader finishDownloadWithErrorString:[NSString stringWithFormat:@"Failed to extract client-overrides from modpack package: %@", error.localizedDescription]];
+        return;
+    }
+
+    // Delete package cache
+    [NSFileManager.defaultManager removeItemAtPath:packagePath error:nil];
+
+    // Download dependency client json (if available)
+    NSDictionary<NSString *, NSString *> *depInfo = [ModpackUtils infoForDependencies:indexDict[@"dependencies"]];
+    if (depInfo[@"json"]) {
+        NSString *jsonPath = [NSString stringWithFormat:@"%1$s/versions/%2$@/%2$@.json", getenv("POJAV_GAME_DIR"), depInfo[@"id"]];
+        NSURLSessionDownloadTask *task = [downloader createDownloadTask:depInfo[@"json"] size:0 sha:nil altName:nil toPath:jsonPath];
+        [task resume];
+    }
+    // TODO: automation for Forge
+
+    // Create profile
+    NSString *tmpIconPath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"icon.png"];
+    PLProfiles.current.profiles[indexDict[@"name"]] = @{
+        @"gameDir": [NSString stringWithFormat:@"./custom_gamedir/%@", destPath.lastPathComponent],
+        @"name": indexDict[@"name"],
+        @"lastVersionId": depInfo[@"id"],
+        @"icon": [NSString stringWithFormat:@"data:image/png;base64,%@",
+            [[NSData dataWithContentsOfFile:tmpIconPath]
+            base64EncodedStringWithOptions:0]]
+    }.mutableCopy;
+    PLProfiles.current.selectedProfileName = indexDict[@"name"];
+}
+
+@end
